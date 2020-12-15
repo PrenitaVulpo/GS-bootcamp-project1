64978995964c0a921e571d08ad825cacceb1ab75
PrenitaVulpo <guilhermepdl97@protonmail.com>
2020-12-15 16:53:32 -0300

    everything configured

---
+++ backend/prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  //faz com que as aspas sejam sempre simples
+  singleQuote: true,
+  //coloca vírgulas sempre depois de alguma variável dentro de um objeto
+  trailingComma: 'all',
+  //evita que parênteses sejam colocados quando uma arrow function tiver só um parâmetro
+  arrowParens: 'avoid'
+}
